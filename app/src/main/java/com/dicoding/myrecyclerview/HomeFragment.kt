7f78d3a326a5375a86ddbe98ce3d98d498234e60
Muhammad Diponegoro <dipoengoro@outlook.com>
2022-04-11 11:52:34 +0700

    Initial commit

--- app/src/main/java/com/dicoding/myrecyclerview/HomeFragment.kt
+++ app/src/main/java/com/dicoding/myrecyclerview/HomeFragment.kt
@@ -5,13 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import com.dicoding.myrecyclerview.databinding.FragmentHomeBinding
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
