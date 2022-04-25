ffed85e05e392147c02ef4e4d6b69706620953da
Muhammad Diponegoro <dipoengoro@outlook.com>
2022-04-25 11:31:13 +0700

    add autotextsize

--- app/src/main/java/com/dicoding/myrecyclerview/ui/NewsFragment.kt
+++ app/src/main/java/com/dicoding/myrecyclerview/ui/NewsFragment.kt
@@ -1,41 +1,32 @@
 package com.dicoding.myrecyclerview.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.myrecyclerview.*
+import com.dicoding.myrecyclerview.R
 import com.dicoding.myrecyclerview.adapters.NewsAdapter
-import com.dicoding.myrecyclerview.data.NewsData
 import com.dicoding.myrecyclerview.databinding.FragmentNewsBinding
-import com.dicoding.myrecyclerview.model.News
 
 
 class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
-    private lateinit var listAdapter: NewsAdapter
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentNewsBinding.inflate(inflater, container, false)
-        listAdapter = NewsAdapter {
-            toDetail(it)
-        }
-        binding.recyclerViewNews.apply {
-            adapter = listAdapter
-            layoutManager = LinearLayoutManager(requireContext())
-        }
-        listAdapter.setList(NewsData.listData)
-        return binding.root
-    }
+        binding.apply {
+            webview.apply {
+                loadUrl(getString(R.string.default_url))
+                settings.javaScriptEnabled = true
+            }
 
-    private fun toDetail(news: News) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerView, NewsDetailFragment(news))
-            .commit()
+            return root
+        }
     }
 }
